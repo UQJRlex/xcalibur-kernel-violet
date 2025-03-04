eedc0fd8b9c4095846bc6cef5adeeb0783397ba1
UQJRlex <xuzhuyin527@gmail.com>
2025-03-04 10:36:22 +0800

    kernel: susfs: import susfs4ksu-4.14-155-250223
    https://gitlab.com/simonpunk/susfs4ksu/-/commit/77905b5a071e6f3669e3b1814cea30147c0801da

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -5,6 +5,10 @@
 #include <linux/seq_file.h>
 #include <asm/setup.h>
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
+#endif
+
 static char proc_cmdline[COMMAND_LINE_SIZE];
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 #include <asm/setup.h>
@@ -32,6 +36,12 @@ static void proc_command_line_init(void) {
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+	if (!susfs_spoof_cmdline_or_bootconfig(m)) {
+		seq_putc(m, '\n');
+		return 0;
+	}
+#endif
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 	seq_printf(m, "%s\n", proc_command_line);
 #else
