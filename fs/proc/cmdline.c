9be54616f8efe4d499994d7fe1fd0c9407fad23a
UQJRlex <xuzhuyin527@gmail.com>
2025-04-03 10:59:46 +0800

    Revert "kernel: susfs: import susfs4ksu-4.14-155-250223"
    This reverts commit 8d8078c2bd4064004dbbe28e1b860b6b849665c2.

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,10 +3,7 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
-
-#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
-extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
-#endif
+#include <asm/setup.h>
 
 static char proc_cmdline[COMMAND_LINE_SIZE];
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
@@ -35,12 +32,6 @@ static void proc_command_line_init(void) {
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
-	if (!susfs_spoof_cmdline_or_bootconfig(m)) {
-		seq_putc(m, '\n');
-		return 0;
-	}
-#endif
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 	seq_printf(m, "%s\n", proc_command_line);
 #else
